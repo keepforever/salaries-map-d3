de4b74ed2b8307054f9095d6584b57946e17cfa9
keepforever <briancilenti@gmail.com>
2019-03-21 15:56:10 -0700

    add bootstrap css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { Container } from "./styled/app"
+import Preloader from "./comps/Preloader"
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
+
+  state = {
+    techSalaries: [],
+  }
+
   render() {
+    const { techSalaries } = this.state;
+
+    if(techSalaries.length < 1) return <Preloader />
+
     return (
       <Container>
-        <h2>Hello App</h2>
+        Hello App
       </Container>
     );
   }
--- src/styled/app.js
+++ src/styled/app.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  height: calc(100vh - 40px);
   display: flex;
   flex-direction: column;
   padding: 20px;
