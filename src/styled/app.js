2ce0dcff94b6e7fac7277590366f0b63c6d002fd
keepforever <briancilenti@gmail.com>
2019-03-21 15:41:18 -0700

    styled components, remove boiler

---
+++ src/styled/app.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: calc(100vh - 40px);
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  color: cornsilk;
+  background: grey;
+`;
