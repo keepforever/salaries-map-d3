78e63f7db8297937dba0ef227cd38cd20f717d09
keepforever <briancilenti@gmail.com>
2019-03-21 17:01:24 -0700

    moving along

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
-import { Container } from "./styled/app";
-import Preloader from "./comps/Preloader";
 import * as d3 from "d3";
-import _ from "lodash";
+// import _ from "lodash";
+// locals
+import Preloader from "./comps/Preloader";
+// import CountyMap from "./components/CountyMap";
+// utils
 import { loadAllData } from "./DataHandling";
 
 class App extends Component {
@@ -16,9 +18,27 @@ class App extends Component {
     loadAllData(data => this.setState(data));
   }
 
+  // In the App class itself, we add a countyValue method. It takes a county entry and a map of tech salaries, and it returns the delta between median household income and a single tech salary.
+
+  countyValue(county, techSalariesMap) {
+    const medianHousehold = this.state.medianIncomes[county.id],
+      salaries = techSalariesMap[county.name];
+
+    if (!medianHousehold || !salaries) {
+      return null;
+    }
+
+    const median = d3.median(salaries, d => d.base_salary);
+
+    return {
+      countyID: county.id,
+      value: median - medianHousehold.medianIncome
+    };
+  }
+
   render() {
     const { techSalaries } = this.state;
-    console.log('this.state = ', this.state, '\n' )
+    console.log("this.state = ", this.state, "\n");
 
     if (techSalaries.length < 1) return <Preloader />;
 
